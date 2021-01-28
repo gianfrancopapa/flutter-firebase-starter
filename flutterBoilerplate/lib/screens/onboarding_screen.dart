533a5221ddcb76f189d1cce62ff1092fe260e6d3
JoaquinMatteo <jmatteo@somniosoftware.com>
2021-01-28 15:28:52 -0300

    FB26 Added new OnboardingScreen model

--- flutterBoilerplate/lib/screens/onboarding_screen.dart
+++ flutterBoilerplate/lib/screens/onboarding_screen.dart
@@ -1,7 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterBoilerplate/screens/login_screen.dart';
-import 'package:sk_onboarding_screen/sk_onboarding_model.dart';
-import 'package:sk_onboarding_screen/sk_onboarding_screen.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:flutterBoilerplate/constants/assets.dart';
 
 class OnBoardingScreen extends StatefulWidget {
@@ -13,45 +13,79 @@ class OnBoardingScreen extends StatefulWidget {
 
 class OnBoardingScreenState extends State<OnBoardingScreen>
     with TickerProviderStateMixin {
-  final pages = [
-    SkOnboardingModel(
-        title: 'Welcome to Flutter Boilerplate',
-        description:
-            'A Somnios\'s applications to to be reused when creating a new app',
-        titleColor: Colors.black,
-        descripColor: const Color(0xFF929794),
-        imagePath: AppAsset.somnioLogo),
-    SkOnboardingModel(
-        title: 'Options',
-        description:
-            'Explore and reuse generic functions to let you start coding, as soon as possible',
-        titleColor: Colors.black,
-        descripColor: const Color(0xFF929794),
-        imagePath: AppAsset.onboarding2),
-    SkOnboardingModel(
-        title: 'Documentation on Confluence',
-        description:
-            'You can find some documentation on Confluence in order to find information about this app',
-        titleColor: Colors.black,
-        descripColor: const Color(0xFF929794),
-        imagePath: AppAsset.onboarding3),
-  ];
+  Widget _buildImage(String assetName) {
+    return Align(
+      child: Image.asset(assetName, width: 350.0),
+      alignment: Alignment.bottomCenter,
+    );
+  }
+
+  PageViewModel _page(
+      {String title, String bodyText, String asset, Color color}) {
+    return PageViewModel(
+      titleWidget: AutoSizeText(
+        title,
+        maxLines: 1,
+        style: const TextStyle(color: Colors.black),
+      ),
+      bodyWidget: AutoSizeText(
+        bodyText,
+        maxLines: 2,
+        style: const TextStyle(
+          color: Color(0xFF929794),
+        ),
+      ),
+      image: _buildImage(asset),
+      decoration: PageDecoration(
+        pageColor: color,
+        imagePadding: EdgeInsets.zero,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SKOnboardingScreen(
-        bgColor: Colors.white,
-        themeColor: const Color(0xFFf74269),
-        pages: pages,
-        skipClicked: (value) {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => LoginScreen()));
-        },
-        getStartedClicked: (value) {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => LoginScreen()));
-        },
+      body: IntroductionScreen(
+        pages: [
+          _page(
+            title: 'Welcome to Flutter Boilerplate',
+            bodyText:
+                'A Somnios\'s applications to to be reused when creating a new app',
+            asset: AppAsset.somnioLogo,
+            color: Colors.white,
+          ),
+          _page(
+            title: 'Options',
+            bodyText:
+                'Explore and reuse generic functions to let you start coding, as soon as possible',
+            asset: AppAsset.onboarding2,
+            color: Colors.white,
+          ),
+          _page(
+            title: 'Documentation on Confluence',
+            bodyText:
+                'You can find some documentation on Confluence in order to find information about this app',
+            asset: AppAsset.onboarding3,
+            color: Colors.white,
+          ),
+        ],
+        onDone: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())),
+        showSkipButton: true,
+        skipFlex: 0,
+        nextFlex: 0,
+        skip: const Text('Skip'),
+        next: const Icon(Icons.arrow_forward),
+        done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
+        dotsDecorator: const DotsDecorator(
+          size: Size(10.0, 10.0),
+          color: Color(0xFFBDBDBD),
+          activeSize: Size(22.0, 10.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0)),
+          ),
+        ),
       ),
     );
   }
