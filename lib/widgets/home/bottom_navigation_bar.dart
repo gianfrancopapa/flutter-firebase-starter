d7243baa2fd5c8219f5cda1ada0f2ce8452ea6a9
Akshar Patel <akshar26@gmail.com>
2021-03-07 18:03:39 +0530

    Remove extra space under bottom nav

--- lib/widgets/home/bottom_navigation_bar.dart
+++ lib/widgets/home/bottom_navigation_bar.dart
@@ -1,15 +1,13 @@
 import 'package:firebasestarter/constants/colors.dart';
-import 'package:firebasestarter/utils/screen_size.dart';
 import 'package:firebasestarter/widgets/common/margin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_icons/flutter_icons.dart';
 
 class BottomNavBar extends StatelessWidget {
-  final _screenSize = ScreenSize();
   final int index;
   final void Function(int) updateIndex;
 
-  BottomNavBar(this.index, this.updateIndex);
+  const BottomNavBar(this.index, this.updateIndex);
 
   Widget _item(int idx, IconData icon) => InkWell(
         onTap: () => updateIndex(idx),
@@ -31,10 +29,8 @@ class BottomNavBar extends StatelessWidget {
               top: BorderSide(color: Color(0xffC1C1C1), width: 1.0),
             ),
           ),
-          height:
-              _screenSize.getSize(context: context, designHeight: 110.0).height,
           child: Padding(
-            padding: const EdgeInsets.only(top: 18.0),
+            padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
