fe4e8a6f1e86decc9e79e52b7d8d8fc713d6422f
Federico Perez <fperez@somniosoftware.com>
2021-03-09 15:24:13 -0300

    Splash screen

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:firebasestarter/constants/assets.dart';
 
 class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Center(
-        child: Text('HOLA'),
+      body: Align(
+        alignment: Alignment.center,
+        child: Image(
+          image: AssetImage(Assets.somnioLogo),
+          height: 220.0,
+          width: 220.0,
+        ),
       ),
+      backgroundColor: Colors.white,
     );
   }
 }
