68b5ef207860f29d6710a6e43d07aa8f0daf3f28
Federico Perez <fperez@somniosoftware.com>
2021-05-31 11:57:04 -0300

    fix/styling changes

--- test/widget/employees/employees_widget_test.dart
+++ test/widget/employees/employees_widget_test.dart
@@ -6,42 +6,44 @@ import 'package:repository/repository.dart';
 import '../test_bench.dart';
 import 'mocks/employees_widget_mocks.dart';
 
-List<EmployeeEntity> employeesList;
-EmployeeEntity testEmployee = EmployeeEntity.fromJson({
-  'firstName': 'testName',
-  'lastName': 'testLastName',
-  'email': 'testEmail',
-  'avatarAsset': 'testAvatar',
-  'role': 'user',
-  'age': 20,
-  'address': 'testAddress',
-  'phoneNumber': '098123456',
-  'description': 'testDescription',
-});
-
 void main() {
+  EmployeesRepository employeesRepository;
+  List<EmployeeEntity> employeesList;
+  EmployeeEntity testEmployee;
+
+  setUp(() {
+    employeesRepository = MockEmployeesRepository();
+    testEmployee = EmployeeEntity.fromJson({
+      'firstName': 'testName',
+      'lastName': 'testLastName',
+      'email': 'testEmail',
+      'avatarAsset': 'testAvatar',
+      'role': 'user',
+      'age': 20,
+      'address': 'testAddress',
+      'phoneNumber': '098123456',
+      'description': 'testDescription',
+    });
+  });
+
   testWidgets('Employees load correctly', (WidgetTester tester) async {
-    final mockEmployeesRepository = MockEmployeesRepository();
     employeesList = [testEmployee];
 
-    when(mockEmployeesRepository.getAll())
-        .thenAnswer((_) async => employeesList);
+    when(employeesRepository.getAll()).thenAnswer((_) async => employeesList);
 
     await tester.pumpApp(TeamScreen(),
-        employeesRepository: mockEmployeesRepository);
+        employeesRepository: employeesRepository);
 
     expect(find.text('testName testLastName'), findsOneWidget);
   });
 
   testWidgets('Employees load empty', (WidgetTester tester) async {
-    final mockEmployeesRepository = MockEmployeesRepository();
     employeesList = [];
 
-    when(mockEmployeesRepository.getAll())
-        .thenAnswer((_) async => employeesList);
+    when(employeesRepository.getAll()).thenAnswer((_) async => employeesList);
 
     await tester.pumpApp(TeamScreen(),
-        employeesRepository: mockEmployeesRepository);
+        employeesRepository: employeesRepository);
 
     expect(find.byType(EmployeesList), findsNothing);
     expect(find.text('No employees found'), findsOneWidget);
