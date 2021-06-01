0f95a655a7dc98ad917f8b91182307f3c0669e1e
Federico Perez <fperez@somniosoftware.com>
2021-06-01 12:49:28 -0300

    feat/New widget tests

--- test/widget/employees/employees_widget_test.dart
+++ test/widget/employees/employees_widget_test.dart
@@ -1,17 +1,33 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:firebasestarter/bloc/employees/employees_bloc.dart';
+import 'package:firebasestarter/bloc/employees/employees_event.dart';
+import 'package:firebasestarter/bloc/employees/employees_state.dart';
+import 'package:firebasestarter/models/employee.dart';
 import 'package:firebasestarter/screens/team/team_screen.dart';
 import 'package:firebasestarter/widgets/team/employees_list.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart' as mocktail;
+
 import 'package:repository/repository.dart';
 import '../test_bench.dart';
-import 'mocks/employees_widget_mocks.dart';
+import '../test_bench_mocks.dart';
+
+class FakeEmployeesEvent extends Fake implements EmployeesEvent {}
+
+class FakeEmployeesState extends Fake implements EmployeesState {}
 
 void main() {
   EmployeesRepository employeesRepository;
-  List<EmployeeEntity> employeesList;
+  List<EmployeeEntity> employeesEntityList;
   EmployeeEntity testEmployee;
+  EmployeesBloc employeesBloc;
 
-  setUp(() {
+  setUpAll(() {
+    mocktail.registerFallbackValue<EmployeesState>(FakeEmployeesState());
+    mocktail.registerFallbackValue<EmployeesEvent>(FakeEmployeesEvent());
+
     employeesRepository = MockEmployeesRepository();
     testEmployee = EmployeeEntity.fromJson({
       'firstName': 'testName',
@@ -24,28 +40,118 @@ void main() {
       'phoneNumber': '098123456',
       'description': 'testDescription',
     });
+    employeesBloc = MockEmployeesBloc();
   });
 
   testWidgets('Employees load correctly', (WidgetTester tester) async {
-    employeesList = [testEmployee];
+    employeesEntityList = [testEmployee];
 
-    when(employeesRepository.getAll()).thenAnswer((_) async => employeesList);
+    when(employeesRepository.getAll())
+        .thenAnswer((_) async => employeesEntityList);
 
     await tester.pumpApp(TeamScreen(),
         employeesRepository: employeesRepository);
 
     expect(find.text('testName testLastName'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+    expect(find.text('No employees found'), findsNothing);
   });
 
   testWidgets('Employees load empty', (WidgetTester tester) async {
-    employeesList = [];
+    employeesEntityList = [];
 
-    when(employeesRepository.getAll()).thenAnswer((_) async => employeesList);
+    when(employeesRepository.getAll())
+        .thenAnswer((_) async => employeesEntityList);
 
     await tester.pumpApp(TeamScreen(),
         employeesRepository: employeesRepository);
 
     expect(find.byType(EmployeesList), findsNothing);
     expect(find.text('No employees found'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets('Bloc emits EmployeesLoadEmpty, screen shows message',
+      (WidgetTester tester) async {
+    const loadingState = EmployeesLoadEmpty();
+    whenListen(
+      employeesBloc,
+      Stream.value(loadingState),
+    );
+
+    await tester.pumpApp(TeamScreen(), employeesBloc: employeesBloc);
+
+    expect(find.byType(EmployeesList), findsNothing);
+    expect(find.text('No employees found'), findsOneWidget);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets('Bloc emits EmployeesLoadSuccess, screen shows EmployeesList',
+      (WidgetTester tester) async {
+    employeesEntityList = [testEmployee];
+    final employeesList = employeesEntityList
+        .map((employee) => Employee.fromEntity(employee))
+        .toList();
+
+    final loadingState = EmployeesLoadSuccess(employeesList);
+
+    whenListen(
+      employeesBloc,
+      Stream.value(loadingState),
+    );
+
+    await tester.pumpApp(TeamScreen(), employeesBloc: employeesBloc);
+
+    expect(find.byType(EmployeesList), findsOneWidget);
+    expect(find.text('No employees found'), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+  });
+
+  testWidgets(
+      'Bloc emits EmployeesInitial, screen shows CircularProgressIndicator',
+      (WidgetTester tester) async {
+    const loadingState = EmployeesInitial();
+    whenListen(
+      employeesBloc,
+      Stream.value(loadingState),
+    );
+
+    await tester.pumpApp(TeamScreen(), employeesBloc: employeesBloc);
+
+    expect(find.byType(EmployeesList), findsNothing);
+    expect(find.text('No employees found'), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets(
+      'Bloc emits EmployeesLoadInProgress, screen shows CircularProgressIndicator',
+      (WidgetTester tester) async {
+    const loadingState = EmployeesLoadInProgress();
+    whenListen(
+      employeesBloc,
+      Stream.value(loadingState),
+    );
+
+    await tester.pumpApp(TeamScreen(), employeesBloc: employeesBloc);
+
+    expect(find.byType(EmployeesList), findsNothing);
+    expect(find.text('No employees found'), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets(
+      'Bloc emits EmployeesLoadFailure, screen shows CircularProgressIndicator',
+      (WidgetTester tester) async {
+    const loadingState = EmployeesLoadFailure('error');
+    whenListen(
+      employeesBloc,
+      Stream.value(loadingState),
+    );
+
+    await tester.pumpApp(TeamScreen(), employeesBloc: employeesBloc);
+
+    expect(find.byType(EmployeesList), findsNothing);
+    expect(find.text('No employees found'), findsNothing);
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
   });
 }
--- test/widget/employees/mocks/employees_widget_mocks.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mockito/mockito.dart';
-import 'package:repository/repository.dart';
-
-class MockEmployeesRepository extends Mock implements EmployeesRepository {}
