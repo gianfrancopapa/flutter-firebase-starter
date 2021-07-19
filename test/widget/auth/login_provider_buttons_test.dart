13eddf641717d4d1261f771c79fe3871927adf2f
Gonzalo Sosa <gonzasosalago@gmail.com>
2021-07-19 14:15:25 -0300

    refactor: test fixed

--- test/widget/auth/login_provider_buttons_test.dart
+++ test/widget/auth/login_provider_buttons_test.dart
@@ -1,4 +1,6 @@
+import 'package:firebasestarter/bloc/login/login_bloc.dart';
 import 'package:firebasestarter/widgets/auth/login_provider_buttons_section.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../test_bench.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -6,16 +8,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'mocks/login_provider_button_mocks.dart';
 
 void main() {
-  testWidgets('Auth service button text', (WidgetTester tester) async {
-    final loginBlocMock = MockLoginBloc();
-    await tester.pumpApp(Material(
-      child: LoginProviderButtonsSection(loginBlocMock),
-    ));
+  group('LoginProviderButtonsSection', () {
+    LoginBloc loginBloc;
 
-    await tester.pump();
-    expect(find.text('Sign in with Google'), findsOneWidget);
-    expect(find.text('Sign in with Facebook'), findsOneWidget);
-    expect(find.text('Sign in with Apple ID'), findsOneWidget);
-    expect(find.text('Sign in anonymously'), findsOneWidget);
+    setUp(() {
+      loginBloc = MockLoginBloc();
+    });
+
+    testWidgets('Auth service button text renders', (tester) async {
+      await tester.pumpApp(
+        Material(
+          child: BlocProvider.value(
+            value: loginBloc,
+            child: const LoginProviderButtonsSection(),
+          ),
+        ),
+      );
+
+      expect(find.text('Sign in with Google'), findsOneWidget);
+      expect(find.text('Sign in with Facebook'), findsOneWidget);
+      expect(find.text('Sign in with Apple ID'), findsOneWidget);
+      expect(find.text('Sign in anonymously'), findsOneWidget);
+    });
   });
 }
