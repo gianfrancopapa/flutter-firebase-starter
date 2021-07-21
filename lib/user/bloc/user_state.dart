48c9c15751d423531c1e73fdd4f91683021407e5
Gonzalo Sosa <gonzasosalago@gmail.com>
2021-07-21 15:16:04 -0300

    refactor: feature driven user added

---
+++ lib/user/bloc/user_state.dart
@@ -0,0 +1,20 @@
+part of 'user_bloc.dart';
+
+enum UserStatus { initial, loading, success, failure }
+
+class UserState extends Equatable {
+  const UserState({@required this.status, this.user}) : assert(status != null);
+
+  final UserStatus status;
+  final User user;
+
+  UserState copyWith({UserStatus status, User user}) {
+    return UserState(
+      status: status ?? this.status,
+      user: user ?? this.user,
+    );
+  }
+
+  @override
+  List<Object> get props => [status, user];
+}
