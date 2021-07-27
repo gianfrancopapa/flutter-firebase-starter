6e9c750b1be4ac7eea61e36c0671b961821808fd
Gonzalo Sosa <gonzasosalago@gmail.com>
2021-07-27 13:33:45 -0300

    feat: screens shown in home refactored to accept a bottom nav bar

--- lib/employees/view/employees_screen.dart
+++ lib/employees/view/employees_screen.dart
@@ -6,7 +6,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class EmployeesScreen extends StatelessWidget {
-  const EmployeesScreen({Key key}) : super(key: key);
+  const EmployeesScreen({Key key, @required this.bottomNavigationBar})
+      : assert(bottomNavigationBar != null),
+        super(key: key);
+
+  final Widget bottomNavigationBar;
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +25,7 @@ class EmployeesScreen extends StatelessWidget {
       body: const _EmployeesList(
         key: Key('employeesScreen_employeesList'),
       ),
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
 }
