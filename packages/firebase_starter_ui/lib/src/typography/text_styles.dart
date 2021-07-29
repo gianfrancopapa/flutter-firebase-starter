2db2ea5102c9dffb6e2c3c5df626f7d79725d01d
Gonzalo Sosa <gonzasosalago@gmail.com>
2021-07-29 16:27:06 -0300

    feat: updated font weight name

--- packages/firebase_starter_ui/lib/src/typography/text_styles.dart
+++ packages/firebase_starter_ui/lib/src/typography/text_styles.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:firebase_starter_ui/firebase_starter_ui.dart';
 import 'package:flutter/widgets.dart';
 
@@ -8,42 +6,42 @@ abstract class FFTextStyle {
   /// Headline 1 Text Style
   static final TextStyle headline1 = _baseTextStyle.copyWith(
     fontSize: 32,
-    fontWeight: FFFontWeight.black,
+    fontWeight: FSFontWeight.black,
     height: 1.25,
   );
 
   /// Headline 2 Text Style
   static final TextStyle headline2 = _baseTextStyle.copyWith(
     fontSize: 26,
-    fontWeight: FFFontWeight.extraBold,
+    fontWeight: FSFontWeight.extraBold,
     height: 1.25,
   );
 
   /// Headline 3 Text Style
   static final TextStyle headline3 = _baseTextStyle.copyWith(
     fontSize: 22,
-    fontWeight: FFFontWeight.extraBold,
+    fontWeight: FSFontWeight.extraBold,
     height: 1.25,
   );
 
   /// Headline 4 Text Style
   static final TextStyle headline4 = _baseTextStyle.copyWith(
     fontSize: 20,
-    fontWeight: FFFontWeight.extraBold,
+    fontWeight: FSFontWeight.extraBold,
     height: 1.25,
   );
 
   /// Headline 5 Text Style
   static final TextStyle headline5 = _baseTextStyle.copyWith(
     fontSize: 18,
-    fontWeight: FFFontWeight.extraBold,
+    fontWeight: FSFontWeight.extraBold,
     height: 1.25,
   );
 
   /// Headline 6 Text Style
   static final TextStyle headline6 = _baseTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FFFontWeight.extraBold,
+    fontWeight: FSFontWeight.extraBold,
     height: 1.25,
   );
 
@@ -70,7 +68,7 @@ abstract class FFTextStyle {
   /// Button Text Style
   static final TextStyle button = _baseTextStyle.copyWith(
     fontSize: 16,
-    fontWeight: FFFontWeight.semiBold,
+    fontWeight: FSFontWeight.semiBold,
   );
 
   /// Caption Text Style
@@ -81,14 +79,14 @@ abstract class FFTextStyle {
   /// Overline Text Style
   static final TextStyle overline = _baseTextStyle.copyWith(
     fontSize: 10,
-    fontWeight: FFFontWeight.medium,
+    fontWeight: FSFontWeight.medium,
   );
 
   static const _baseTextStyle = TextStyle(
     fontFamily: 'Poppins',
     package: 'firebase_starter_ui',
     color: FSColors.darkBlue,
-    fontWeight: FFFontWeight.regular,
+    fontWeight: FSFontWeight.regular,
     height: 1.5,
   );
 }
