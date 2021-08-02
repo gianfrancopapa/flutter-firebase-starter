df82b10ba656e08b84267dfcfad54332bed36159
Gonzalo Sosa <gonzasosalago@gmail.com>
2021-08-02 15:39:30 -0300

    feat: app bloc test layer tested

---
+++ test/app/bloc/app_event_test.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:firebasestarter/app/app.dart';
+import 'package:firebasestarter/models/user.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('AppEvent', () {
+    group('AppIsFirstTimeLaunched', () {
+      test('supports value comparison', () {
+        expect(AppIsFirstTimeLaunched(), equals(AppIsFirstTimeLaunched()));
+      });
+    });
+
+    group('AppUserChanged', () {
+      test('throwsAssertionError when user is null', () {
+        expect(() => AppUserChanged(user: null), throwsAssertionError);
+      });
+
+      test('supports value comparison', () {
+        expect(
+          AppUserChanged(user: User.empty),
+          equals(
+            AppUserChanged(user: User.empty),
+          ),
+        );
+      });
+    });
+
+    group('AppLogoutRequsted', () {
+      test('supports value comparison', () {
+        expect(AppLogoutRequsted(), equals(AppLogoutRequsted()));
+      });
+    });
+  });
+}
